b04c4db17cda5aa763c4a2e91196d72b399e644a
cloke <cory@me.com>
2020-12-19 13:03:08 -0500

    Initial Commit

---
+++ Shared/KitchenSinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  KitchenSinkApp.swift
+//  Shared
+//
+//  Created by Cory Loken on 12/19/20.
+//
+
+import SwiftUI
+
+@main
+struct KitchenSinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
