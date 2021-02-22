1b4be52c539b09593045018005e5f99aaf8f66f5
cloke <cory@me.com>
2021-02-22 18:03:12 -0500

    There seems to be a bug when the commit has added files and changes

--- Local Packages/Git/Sources/Git/StashListView.swift
+++ Local Packages/Git/Sources/Git/StashListView.swift
@@ -1,8 +1,44 @@
 //
-//  File.swift
+//  StashListView.swift
 //  
 //
 //  Created by Cory Loken on 2/22/21.
 //
 
-import Foundation
+import SwiftUI
+
+struct StashListView: View {
+  public let repository: Repository
+  @State private var stashes = [String]()
+  @State private var isExpanded = false
+  
+  public init(repository: Repository) {
+    self.repository = repository
+  }
+  
+  var body: some View {
+    DisclosureGroup(isExpanded: $isExpanded) {
+      List(stashes, id: \.self) {
+        Text($0)
+      }
+      .onChange(of: repository.id, perform: { value in
+        ViewModel.shared.stashList() { self.stashes = $0 }
+      })
+      .onChange(of: isExpanded, perform:  { value in
+        if isExpanded == true {
+          ViewModel.shared.stashList() { self.stashes = $0 }
+        }
+      })
+    } label: {
+      HStack {
+        Text("Stash")
+        Spacer()
+        Button {
+          ViewModel.shared.stashList() { self.stashes = $0 }
+        } label: {
+          Image(systemName: "arrow.counterclockwise.icloud")
+        }
+      }
+    }
+  }
+}
--- Shared/Extensions/Color.swift
+++ Shared/Extensions/Color.swift
@@ -5,4 +5,14 @@
 //  Created by Cory Loken on 2/22/21.
 //
 
-import Foundation
+import SwiftUI
+
+extension Color {
+  var isDarkColor: Bool {
+    var r, g, b, a: CGFloat
+    (r, g, b, a) = (0, 0, 0, 0)
+    NSColor(self).usingColorSpace(.extendedSRGB)?.getRed(&r, green: &g, blue: &b, alpha: &a)
+    let lum = 0.2126 * r + 0.7152 * g + 0.0722 * b
+    return  lum < 0.50
+  }
+}
