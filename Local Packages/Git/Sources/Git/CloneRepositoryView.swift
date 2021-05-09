550aea2a96fc3f2e196839ce6bc7eeb6b2149565
cloke <cory@me.com>
2021-05-09 18:30:08 -0400

    Still a bug that prevents a new file from being added and committed

--- Local Packages/Git/Sources/Git/CloneRepositoryView.swift
+++ Local Packages/Git/Sources/Git/CloneRepositoryView.swift
@@ -1,20 +1,36 @@
 //
-//  SwiftUIView.swift
-//  
+//  CloneRepositoryView.swift
+//
 //
 //  Created by Cory Loken on 5/9/21.
 //
 
 import SwiftUI
 
-struct SwiftUIView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+public struct CloneRepositoryView: View {
+  @StateObject private var viewModel: ViewModel = .shared
+  @State private var cloneUrl = ""
+  @Binding private var isCloning: Bool
+  
+  public init(isCloning: Binding<Bool>) {
+    self._isCloning = isCloning
+  }
+  
+  public var body: some View {
+    Form {
+      Section(header: Text("Repository Url")) {
+        TextField("", text: $cloneUrl)
+        HStack {
+          Button { isCloning = false }
+            label: { Text("Cancel") }
+          Spacer()
+          Button {
+            viewModel.cloneRepository(with: cloneUrl) {
+              isCloning = false
+            }
+          } label: { Text("Clone") }
+        }
+      }
     }
-}
-
-struct SwiftUIView_Previews: PreviewProvider {
-    static var previews: some View {
-        SwiftUIView()
-    }
+  }
 }
