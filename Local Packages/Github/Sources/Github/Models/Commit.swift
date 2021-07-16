582ff0256b106def6be130b4c27abbfd65146c23
cloke <cory@me.com>
2021-07-16 18:15:03 -0400

    Look at last commit

--- Local Packages/Github/Sources/Github/Models/Commit.swift
+++ Local Packages/Github/Sources/Github/Models/Commit.swift
@@ -1,8 +1,39 @@
 //
-//  File.swift
-//  File
+//  Commit.swift
+//  Commit
 //
 //  Created by Cory Loken on 7/16/21.
 //
 
-import Foundation
+extension Github {
+  public struct CommitSnapshot: Codable {
+    // TODO: These are stripped down users. If we use the user property we'd have to make everything optional
+//    public var author: User?
+//    public var committer: User?
+    public var message: String
+//      "tree": {
+//        "sha": "f77d0873d3e95f7e19cd74715b9f7333b1fc04af",
+//        "url": "https://api.github.com/repos/tuitionio/vue-tio-modal/git/trees/f77d0873d3e95f7e19cd74715b9f7333b1fc04af"
+//      },
+//      "url": "https://api.github.com/repos/tuitionio/vue-tio-modal/git/commits/118c129254390c203c3e778569a7880eeb212645",
+//      "comment_count": 0,
+//      "verification": {
+//        "verified": true,
+//        "reason": "valid",
+//        "signature": "-----BEGIN PGP SIGNATURE-----\n\nwsBcBAABCAAQBQJbGwWOCRBK7hj4Ov3rIwAAdHIIAA9UkscE1YPkXb/vIdZ7xnww\nV01eYvbC1tifTZeS61HnKy345xCq7dHvCNfU+AyGcD+pcY7YcOR2fJTmT0hYkFyb\nFrxe4I71yBOhmSI3Pjzzr2pepvvR2zYfN+qPs7GuYajyijQWI2Ia/AtoagB583LT\nsTAh1aPddWUDK/9pVX/VKznVCCQ92n6vOfsr2Mhb+x5S5D8cuUvHMaHCVXnPEe7u\nMrXaLFvdYohucq4LzwfsmYkcq2UA2ZfIWaCT3Q+m4GgMaiiHM2I4P/7oRaHyqAba\nvuvraa9mKJQxdm2IrdDlFjZmEKQD/7ASHXwzCmWO125d7+FFZLmaSU2ejezhd/o=\n=/UBD\n-----END PGP SIGNATURE-----\n",
+//        "payload": "tree f77d0873d3e95f7e19cd74715b9f7333b1fc04af\nauthor Artur Grigio <fsastudent@yahoo.com> 1528497550 -0700\ncommitter GitHub <noreply@github.com> 1528497550 -0700\n\nInitial commit"
+//      }
+
+  }
+  public struct Commit: Codable {
+    public var sha: String
+    public var node_id: String
+    public var commit: CommitSnapshot
+    public var url: String
+    public var html_url: String
+    public var comments_url: String
+    public var author: User?
+    public var committer: User?
+//    "parents": []
+  }
+}
--- Local Packages/Github/Sources/Github/RepositoryView.swift
+++ Local Packages/Github/Sources/Github/RepositoryView.swift
@@ -1,15 +1,15 @@
 //
-//  RepositorView.swift
-//  RepositorView
+//  RepositoryView.swift
+//  RepositoryView
 //
 //  Created by Cory Loken on 7/16/21.
 //
 
 import SwiftUI
 extension Github {
-  struct RepositorView: View {
+  struct RepositoryView: View {
     public let organization: String
-    public let repository: String
+    public let repository: Repository
     
     // TODO: make this reference an enum
     @State private var currentTab = "Pulls"
@@ -17,14 +17,26 @@ extension Github {
     var body: some View {
       VStack {
         HStack {
-          Button("Pulls") { currentTab = "Pulls" }
-          Button("Commits") { currentTab = "Commits" }
+          Button {
+            currentTab = "Pulls"
+          } label:  {
+            Text("Pulls")
+              .fontWeight(currentTab == "Pulls" ? .bold : .none)
+          }
+          
+          Button {
+            currentTab = "Commits"
+          } label: {
+            Text("Commits")
+              .fontWeight(currentTab == "Commits" ? .bold : .none)
+          }
         }
+        .buttonStyle(.borderless)
         switch currentTab {
         case "Pulls":
-          PullRequestsView(organization: organization, repository: repository)
+          PullRequestsView(organization: organization, repository: repository.name)
         case "Commits":
-          Text("Display commit logs here")
+          CommitsView(organization: organization, repository: repository)
         default:
           Text("Something is wrong")
         }
@@ -32,6 +44,37 @@ extension Github {
       Spacer()
     }
   }
+  
+  struct CommitsView: View {
+    public let organization: String
+    public let repository: Repository
+    
+    @EnvironmentObject var viewModel: ViewModel
+    @State private var commits = [Github.Commit]()
+    
+    var body: some View {
+      List(commits, id: \.sha) { commit in
+        VStack {
+          VStack {
+            HStack(alignment: .top) {
+              Text(commit.author?.login ?? "Unknown Login")
+              Text(commit.commit.message)
+              Spacer()
+            }
+          }
+          Divider()
+        }
+      }
+      .onAppear {
+        Github.commits(from: repository) {
+          commits = $0
+        } error: {
+          print($0)
+        }
+      }
+    }
+  }
+  
 }
 
 //struct RepositoryView_Previews: PreviewProvider {
