e87ae126844e7ecfa2c9bd9d5e90d98c591621cd
cloke <cory@me.com>
2021-07-16 22:41:36 -0400

    See last commit

--- Local Packages/Github/Sources/Github/CommitsListView.swift
+++ Local Packages/Github/Sources/Github/CommitsListView.swift
@@ -1,8 +1,42 @@
 //
-//  File.swift
-//  File
+//  CommitsListView.swift
+//  CommitsListView
 //
 //  Created by Cory Loken on 7/16/21.
 //
 
-import Foundation
+import SwiftUI
+
+extension Github {
+  struct CommitsListView: View {
+    public let organization: String
+    public let repository: Repository
+    
+    @EnvironmentObject var viewModel: ViewModel
+    @State private var commits = [Github.Commit]()
+    
+    var body: some View {
+      List(commits, id: \.sha) { commit in
+        VStack {
+          VStack {
+            HStack(alignment: .top) {
+              Text(commit.author?.login ?? "Unknown Login")
+              Text(commit.commit.message)
+              Spacer()
+              
+              Text(commit.commit.author.dateFormated)
+            }
+          }
+          Divider()
+        }
+      }
+      .onAppear {
+        Github.commits(from: repository) {
+          commits = $0
+        } error: {
+          print($0)
+        }
+      }
+    }
+  }
+}
